eb30d838e7c25d8591a1dacbf42b9fe5a70faa36
Mathew Perkins <mathewperkins@Mathews-MacBook-Pro.local>
2024-02-25 00:29:03 +1100

    Feat: bump script release

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 GITHUB_REPO="mperkins808/magic-mirror"
 BINARY_NAME="magicmirror"
-TAG="0.0.2"
+TAG="0.0.3"
 BINARY_URL="https://github.com/$GITHUB_REPO/releases/download/$TAG/$BINARY_NAME"
 
 TEMP_DIR=$(mktemp -d)
